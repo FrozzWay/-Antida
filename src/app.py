2cebcf779fcf6df663bc9f58b8d6b19ed93b5f82
Aleksandr Okunev <frozz.way@mail.ru>
2020-05-27 19:54:29 +0500

    fixed, added view user

--- src/app.py
+++ src/app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from src.database import db
 
-from src.blueprints.register import bp as reg_bp
+from src.blueprints.users import bp as users_bp
 from src.blueprints.auth import bp as auth_bp
 
 app = Flask(__name__)
 
 db.init_app(app)
 
-app.register_blueprint(reg_bp)
+app.register_blueprint(users_bp, url_prefix="/users")
 app.register_blueprint(auth_bp, url_prefix="/auth")
 
 app.secret_key = b'_5#y2L"F4ffQ8z\n\xec]/k'
--- src/blueprints/users.py
+++ src/blueprints/users.py
@@ -2,20 +2,23 @@ import sqlite3
 from flask import (
     Blueprint,
     request,
-    jsonify
+    jsonify,
+    session
 )
+from flask.views import MethodView
 from src.database import db
 from werkzeug.security import generate_password_hash
 
-bp = Blueprint('register', __name__)
+bp = Blueprint('users', __name__)
 
 
-@bp.route('/users', methods=['POST'])
+@bp.route('/', methods=["POST"])
 def register():
     request_json = request.json
 
     is_seller = request_json.get('is_seller')
     is_seller = True if is_seller else False
+
     user= {
         "email": request_json.get('email'),
         "password": request_json.get('password'),
@@ -48,6 +51,7 @@ def register():
             (user['email'],)
         )
         account_id = cursor.fetchone()['id']
+        user['id'] = account_id
         print(account_id)
         cursor.execute(
             'INSERT INTO seller (zip_code, street, home, phone, city_id, account_id) '
@@ -56,3 +60,37 @@ def register():
         )
         con.commit()
     return jsonify(user), 200
+
+
+class UsersView(MethodView):
+    def get(self, id):
+        user_id = session.get('user_id')
+        if user_id is None:
+            return '', 403
+        con = db.connection
+        cursor = con.execute(
+            'SELECT * '
+            'FROM account '
+            'WHERE account.id = ?',
+            (id,)
+        )
+        account = dict(cursor.fetchone())
+        account['is_seller'] = False
+        del(account['password'])
+        cursor.execute(
+            'SELECT zip_code, street, phone, home '
+            'FROM seller '
+            'WHERE seller.account_id= ?',
+            (account['id'],)
+        )
+        seller = cursor.fetchone()
+        if seller is None:
+            return jsonify(account), 200
+        seller = dict(seller)
+        account.update(seller)
+        account['is_seller'] = True
+        return jsonify(account), 200
+
+
+
+bp.add_url_rule('/<int:id>', view_func=UsersView.as_view('users'))
